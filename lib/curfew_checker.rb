d46106a697b0faab3b66a453eea32bd75d2e602c
frisaac <frisaac@stu.naperville203.org>
2018-08-08 14:05:46 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,43 @@
 def simple_curfew_checker(time)
-  # code goes here
+  # 
 end
 
 def curfew_checker(time)
   # code goes here
+  if time <= 11
+    "remember, curfew is at 11!"
 end
 
 def complex_curfew_checker(time)
   # code goes here
+  if time < 11
+    puts "remember curfew is at 11!"
+  if time == 11
+    puts "apparate home now!"
+  else
+    puts "you're late!"
 end
 
 def deluxe_curfew_checker(time)
+  curfew = 11
+
   # code goes here
+  if time < curfew
+    puts "you have " + curfew - time +" hours until curfew"
+  elsif time == curfew
+    puts "go home"
+  else
+    puts "you're late!"
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
+  if time < curfew
+    puts "you have " + curfew - time +" hours until curfew"
+  elsif time == curfew
+    puts "go home"
+  else
+    puts "you're late!"
+  end
 end
+
